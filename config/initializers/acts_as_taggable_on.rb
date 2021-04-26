55159501eb77f36a203c69dc995d83f05a517310
KrunaL Parvadiya <krunalcp@gmail.com>
2021-04-26 16:40:25 +0530

    Missing Part of intial commit

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -2,17 +2,3 @@ require "acts-as-taggable-on"
 
 ActsAsTaggableOn.strict_case_match = true
 ActsAsTaggableOn.force_lowercase = false
-
-module ActsAsTaggableOn
-  class Tag < ::ActiveRecord::Base
-    Tag.class_eval do
-      _validators.reject! { |key, _| key == :name }
-
-      _validate_callbacks.each do |callback|
-        callback.raw_filter.attributes.delete :name if callback.raw_filter
-          .is_a?(ActiveRecord::Validations::UniquenessValidator)
-      end
-      validates_uniqueness_of :name, scope: [:site_id, :context]
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+s1 = Site.create(name: 'KrunaL')
+s2 = Site.create(name: 'Purv')
+s3 = Site.create(name: 'Divy')
+
+Project.create(site_id: 1, name: 'K Project1')
+Project.create(site_id: 1, name: 'K Project2')
+Project.create(site_id: 1, name: 'K Project3')
+
+Project.create(site_id: 2, name: 'P Project1')
+Project.create(site_id: 2, name: 'P Project2')
+Project.create(site_id: 2, name: 'P Project3')
+
+Project.create(site_id: 3, name: 'D Project1')
+Project.create(site_id: 3, name: 'D Project2')
+Project.create(site_id: 3, name: 'D Project3')
+
+User.create(site_id: 1, name: 'K User1')
+User.create(site_id: 1, name: 'K User2')
+User.create(site_id: 1, name: 'K User3')
+
+User.create(site_id: 2, name: 'P User1')
+User.create(site_id: 2, name: 'P User2')
+User.create(site_id: 2, name: 'P User3')
+
+User.create(site_id: 3, name: 'D User1')
+User.create(site_id: 3, name: 'D User2')
+User.create(site_id: 3, name: 'D User3')
